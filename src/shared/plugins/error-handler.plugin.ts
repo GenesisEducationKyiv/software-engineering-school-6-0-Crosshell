721cc1109b7788e239c6eb242c66eddd0575d8fb
Crosshell <confighell@gmail.com>
2026-04-12 17:12:37 +0300

    refactor(error-handler): add `Constructor` type and improve type safety

--- src/shared/plugins/error-handler.plugin.ts
+++ src/shared/plugins/error-handler.plugin.ts
@@ -9,20 +9,22 @@ import {
 } from '@/shared/errors/app.errors';
 import { HttpStatus } from '@/shared/constants/http-status.constant';
 
-const STATUS_MAP = new Map<Function, number>([
+type Constructor = abstract new (...args: never[]) => object;
+
+const STATUS_MAP = new Map<Constructor, number>([
   [UnauthorizedError, HttpStatus.UNAUTHORIZED],
   [NotFoundError, HttpStatus.NOT_FOUND],
   [ConflictError, HttpStatus.CONFLICT],
   [RateLimitError, HttpStatus.TOO_MANY_REQUESTS],
 ]);
 
-const STATUS_CACHE = new WeakMap<Function, number>();
+const STATUS_CACHE = new WeakMap<Constructor, number>();
 
 function resolveStatus(error: AppError): number {
   const ctor = error.constructor;
 
-  if (STATUS_CACHE.has(ctor)) {
-    return STATUS_CACHE.get(ctor)!;
+  if (STATUS_CACHE.has(<Constructor>ctor)) {
+    return STATUS_CACHE.get(<Constructor>ctor)!;
   }
 
   let proto = Object.getPrototypeOf(error);
@@ -30,7 +32,7 @@ function resolveStatus(error: AppError): number {
     const status = STATUS_MAP.get(proto.constructor);
 
     if (status !== undefined) {
-      STATUS_CACHE.set(ctor, status);
+      STATUS_CACHE.set(<Constructor>ctor, status);
       return status;
     }
 
@@ -38,7 +40,7 @@ function resolveStatus(error: AppError): number {
   }
 
   const defaultStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-  STATUS_CACHE.set(ctor, defaultStatus);
+  STATUS_CACHE.set(<Constructor>ctor, defaultStatus);
   return defaultStatus;
 }
 
