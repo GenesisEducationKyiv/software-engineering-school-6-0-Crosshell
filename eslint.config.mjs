5686a38edf7874e4fa9e17b46479728146cb6b27
Crosshell <confighell@gmail.com>
2026-05-04 02:00:47 +0300

    feat(hw1): add eslint configuration with prettier and vitest plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,46 @@
 import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import vitestPlugin from '@vitest/eslint-plugin';
 
 export default tseslint.config(
   { ignores: ['dist/**', 'node_modules/**', 'drizzle/**'] },
-  tseslint.configs.recommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
-        { argsIgnorePattern: '^_' },
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
       '@typescript-eslint/consistent-type-imports': [
         'error',
         { prefer: 'type-imports' },
       ],
-      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowNumber: true,
+        },
+      ],
     },
   },
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts'],
+    plugins: { vitest: vitestPlugin },
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'vitest/unbound-method': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+    },
+  },
+  eslintConfigPrettier,
 );
