aedf295d684c98d8d0a85639002fb9cb0240b056
Crosshell <confighell@gmail.com>
2026-05-19 16:29:38 +0300

    test(hw4): update GitHub client tests to use caching decorator and fix mock server path

--- tests/integration/helpers/github.helper.ts
+++ tests/integration/helpers/github.helper.ts
@@ -1,9 +1,13 @@
 import type Redis from 'ioredis';
 import { CacheService } from '@/infrastructure/cache/cache.service';
 import { GithubHttpClient } from '@/modules/github/github-http-client';
+import { CachingGithubHttpClientDecorator } from '@/modules/github/decorators/caching-github-http-client.decorator';
 import { GithubAdapter } from '@/modules/github/github.adapter';
 
 export function createGithubClient(redisClient: Redis): GithubAdapter {
   const cache = new CacheService(redisClient);
-  return new GithubAdapter(new GithubHttpClient(cache));
+
+  return new GithubAdapter(
+    new CachingGithubHttpClientDecorator(new GithubHttpClient(), cache),
+  );
 }
